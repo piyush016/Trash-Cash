85da22b9ece0ea2b0ceb6f895f684dbedbbb4f8b
piyush016 <piyushcool116@gmail.com>
2024-02-27 00:42:01 +0530

    Setting up routes & database

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = require("./config/db");
+
+const userRouter = require("./routes/user");
+const accountRouter = require("./routes/account");
+
+app.use("/user", userRouter);
+app.use("/account", accountRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
