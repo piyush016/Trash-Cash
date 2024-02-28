978d98cc0de4261bca730d8d2d69fda0400a747e
piyush016 <piyushcool116@gmail.com>
2024-02-28 19:41:27 +0530

    App and index

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Signup from "./screens/Signup";
+import Signin from "./screens/Signin";
+import Dashboard from "./screens/Dashboard";
+import SendMoney from "./screens/SendMoney";
+import SideNavigation from "./components/SideNavigation";
+import Nav from "./components/Nav";
+import Foot from "./components/Foot";
+import { Layout } from "antd";
+const { Content } = Layout;
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Nav />
+        <Layout>
+          <SideNavigation />
+          <Content style={{ padding: "8px" }}>
+            <Routes>
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/signin' element={<Signin />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+              <Route path='/send-money' element={<SendMoney />} />
+            </Routes>
+          </Content>
+        </Layout>
+        <Foot />
+      </Layout>
+    </BrowserRouter>
+  );
 }
 
 export default App;
