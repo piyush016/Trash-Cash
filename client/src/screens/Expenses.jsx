d35905730942a43f6314cf0cbaf5a87398d0ad4c
piyush016 <piyushcool116@gmail.com>
2024-03-17 15:10:51 +0530

    Code refactor

--- client/src/screens/Expenses.jsx
+++ client/src/screens/Expenses.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
-import { Select, Divider, Typography } from "antd";
+import { Select, Divider, Typography, Row, Col } from "antd";
 import BarGraph from "../components/BarGraph";
 import LineChart from "../components/LineChart";
 const { Title } = Typography;
 
 const Expenses = () => {
-  const [timeFrame, setTimeFrame] = useState("day");
+  const [timeFrameBar, setTimeFrameBar] = useState("day");
+  const [timeFrameLine, setTimeFrameLine] = useState("day");
   const [expenseFrame, setExpenseFrame] = useState("both");
 
-  const handleTimeFrameChange = (value) => {
-    setTimeFrame(value);
+  const handleTimeFrameChangeBar = (value) => {
+    setTimeFrameBar(value);
+  };
+
+  const handleTimeFrameChangeLine = (value) => {
+    setTimeFrameLine(value);
   };
 
   const handleExpenseFrameChange = (value) => {
@@ -17,47 +22,55 @@ const Expenses = () => {
   };
 
   return (
-    <div>
+    <>
       <Divider orientation='left'>
         <Title level={3} style={{ fontSize: "24px" }}>
           Expenses
         </Title>
       </Divider>
-      <Select
-        popupMatchSelectWidth={80}
-        value={timeFrame}
-        onChange={handleTimeFrameChange}
-        options={[
-          { value: "day", label: <span>Day</span> },
-          { value: "week", label: <span>Week</span> },
-          { value: "month", label: <span>Month</span> },
-          { value: "year", label: <span>Year</span> },
-        ]}
-      />
-      <BarGraph timeFrame={timeFrame} />
-      <Select
-        popupMatchSelectWidth={80}
-        value={expenseFrame}
-        onChange={handleExpenseFrameChange}
-        options={[
-          { value: "both", label: <span>Both</span> },
-          { value: "credit", label: <span>Credit</span> },
-          { value: "debit", label: <span>Debit</span> },
-        ]}
-      />
-      <Select
-        popupMatchSelectWidth={80}
-        value={timeFrame}
-        onChange={handleTimeFrameChange}
-        options={[
-          { value: "day", label: <span>Day</span> },
-          { value: "week", label: <span>Week</span> },
-          { value: "month", label: <span>Month</span> },
-          { value: "year", label: <span>Year</span> },
-        ]}
-      />
-      <LineChart timeFrame={timeFrame} option={expenseFrame} />
-    </div>
+      <Row gutter={[16, 16]}>
+        <Col xs={24} lg={11}>
+          <Title level={4}>Bar Graph</Title>
+          <Select
+            value={timeFrameBar}
+            onChange={handleTimeFrameChangeBar}
+            options={[
+              { value: "day", label: <span>Day</span> },
+              { value: "week", label: <span>Week</span> },
+              { value: "month", label: <span>Month</span> },
+              { value: "year", label: <span>Year</span> },
+            ]}
+          />
+          <BarGraph timeFrame={timeFrameBar} />
+        </Col>
+        <Divider type='vertical' style={{ height: "100%" }} />
+        <Col xs={24} lg={11}>
+          <Title level={4}>Line Chart</Title>
+          <Select
+            style={{ marginRight: "2px" }}
+            value={expenseFrame}
+            onChange={handleExpenseFrameChange}
+            options={[
+              { value: "both", label: <span>Both</span> },
+              { value: "credit", label: <span>Credit</span> },
+              { value: "debit", label: <span>Debit</span> },
+            ]}
+          />
+          <Select
+            style={{ marginLeft: "2px" }}
+            value={timeFrameLine}
+            onChange={handleTimeFrameChangeLine}
+            options={[
+              { value: "day", label: <span>Day</span> },
+              { value: "week", label: <span>Week</span> },
+              { value: "month", label: <span>Month</span> },
+              { value: "year", label: <span>Year</span> },
+            ]}
+          />
+          <LineChart timeFrame={timeFrameLine} option={expenseFrame} />
+        </Col>
+      </Row>
+    </>
   );
 };
 
